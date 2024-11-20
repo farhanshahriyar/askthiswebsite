64c21975b627d5512b79ad7234fc80566d0d127e
farhanshahriyar <farhan.abir9999@gmail.com>
2024-11-20 18:13:40 +0600

    solved the rest bugs

--- src/app/api/chat-stream/route.ts
+++ src/app/api/chat-stream/route.ts
@@ -1,4 +1,4 @@
-import { regChat } from "@/lib/rag-chat";
+import { ragChat } from "@/lib/rag-chat";
 import { aiUseChatAdapter } from "@upstash/rag-chat/nextjs";
 import { NextRequest } from "next/server";
 
@@ -6,7 +6,7 @@ export const POST = async (req: NextRequest) => {
     const { messages, sessionId } = await req.json()
 
     const lastMessage = messages[messages.length - 1].content 
-    const response = await regChat.chat(lastMessage, {streaming: true, sessionId})
+    const response = await ragChat.chat(lastMessage, {streaming: true, sessionId})
     // console.log('response', response);
 
     return aiUseChatAdapter(response)
