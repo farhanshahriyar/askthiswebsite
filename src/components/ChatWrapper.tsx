92dc866cc0cfb11de8c21491f42223fcdd9b64b7
farhanshahriyar <farhan.abir9999@gmail.com>
2024-12-05 23:18:14 +0600

    improved ui and added some functionalities

--- src/components/ChatWrapper.tsx
+++ src/components/ChatWrapper.tsx
@@ -1,6 +1,8 @@
 "use client"
 
 import { useChat } from "ai/react"
+import { Messages } from "./Messages"
+import { ChatInput } from "./ChatInput"
 
 export const ChatWrapper = ({ sessionId }: { sessionId: string }) => {
     const { messages, handleInputChange, handleSubmit, input } = useChat({
@@ -10,14 +12,17 @@ export const ChatWrapper = ({ sessionId }: { sessionId: string }) => {
 
     return (
         <div className="relative min-h-full bg-zinc-900 flex divide-y divide-zinc-700 flex-col justify-between gap-2">
-            <div className="flex-1 text-black bg-zinc-800 justify-between flex flex-col">
+            <div className="flex-1 text-white bg-zinc-800 justify-between flex flex-col">
                 {JSON.stringify(messages)}
+                <Messages messages = {messages} />
             </div>
 
-            <form onSubmit={handleSubmit}>
+            {/* <form onSubmit={handleSubmit}>
             <input className="text-black" value={input} onChange={handleInputChange} type="text"/>
             <button type="submit">Send</button>
-            </form>
+            </form> */}
+
+            <ChatInput />
         </div>
     )
 }
